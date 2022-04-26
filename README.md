a707eb004e61259cea97a6668bf0aa47b5db28d0
Sebastian Barroso <sebbarroso@teco.com.ar>
2022-04-26 16:16:44 -0300

    feat: adding e2e test for put and delete todo

--- README.md
+++ README.md
@@ -1,24 +1,48 @@
 
+# rest-api-test
+
+[API Demo](https://rest-api-test-01.herokuapp.com)
+
+[OpenAPI - Swagger UI](https://rest-api-test-01.herokuapp.com/api/docs/)
+
+[OpenAPI - Formato JSON](https://rest-api-test-01.herokuapp.com/api/docs.json)
+
+### Entorno
+Segun como se encuentre definida la variable `NODE_ENV` se conectará a cada tipo de DB:
+- `NODE_ENV=development`: DB sqlite3
+- `NODE_ENV=test`: DB sqlite3
+- `NODE_ENV=production`: DB postgreSQL
+
 #### Instalación de dependencias
 ```
-npm i
+npm i                # Instalación de dependencias
+cp ejemplo.env .env  # Variables de entorno incluida NODE_ENV
 ```
 
-#### Renombrar .env
+#### DB local para desarrollo y testing
+Para la creación local la variable `NODE_ENV=development`
 ```
-mv ejemplo.env .env
+npm run db:migrate   # Crea la tabla to_do
+npm run db:seed      # Inserta 3 registros en la DB
+```
+En el caso de utilizar `NODE_ENV=production`, se debe completar los datos en el `.env` para realizar la conexión a la DB
+
+#### Test scripts
+Todos usan jest
+```
+npm run test         # solo jest
+npm run test:w       # watch
+npm run test:o       # detectOpenHandles
+npm run test:v       # verbose
+npm run test:s       # silent
 ```
 
-#### Creación de DB
+#### Dev scripts
 ```
-npm run migrate
-```
-
-#### Carga de datos en DB
-```
-npm run seed
+npm run dev          # nodemon
 ```
 
-[Demo](https://demo-rest-server.herokuapp.com/api/todo)
-
-[OpenAPI](https://demo-rest-server.herokuapp.com/api/docs/)
+#### Start script
+```
+npm start            # start
+```
--- tests/e2e/todo.test.js
+++ tests/e2e/todo.test.js
@@ -8,8 +8,13 @@ const { myServer } = require("../../index");
 const { app, paths, server } = myServer;
 const supertest = require("supertest");
 
+const db = require("../../lib/models/db");
 const { ToDo } = require("../../lib/models/db");
+
 let toDo;
+const invalidId = "abc";
+const notFoundId = 100000;
+
 const mockToDoPayloads = [
   {
     name: "Sebato",
@@ -20,53 +25,67 @@ const mockToDoPayloads = [
   {
     name: "Test",
     description: "Test",
-    date: new Date(),
+    date: new Date().toISOString(),
     author: "correo@dominio.com"
   }
 ];
+const mockExpectedErrors = [
+  {
+    error: {
+      message: expect.any(String),
+      status: expect.any(Number),
+      innerErrors: expect.anything()
+    }
+  },
+  {
+    error: { message: "not_found_todo", status: 404 }
+  }
+];
 
-const todoDAO = require("../../lib/DAO/todo");
-jest.mock("../../lib/DAO/todo", () => {
-  const originalModule = jest.requireActual("../../lib/DAO/todo");
+const todoDAO = require("../../lib/services/todo");
+jest.mock("../../lib/services/todo", () => {
+  const originalModule = jest.requireActual("../../lib/services/todo");
   return {
     __esModule: true,
     ...originalModule,
-    updateToDo: jest.fn(() => ({
-      id: 1,
-      ...mockToDoPayloads[0]
-    }))
+    updateToDo: jest.fn(originalModule.updateToDo),
+    deleteTodo: jest.fn(originalModule.deleteTodo)
   };
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
 });
 
 describe("todo", () => {
   describe("get todo route", () => {
-    describe("given the todos do not exist", () => {
+    describe("given the todo do not exist", () => {
       it("should return 404", async () => {
         const toDoId = 1000;
         await supertest(app).get(`${paths.todo}/${toDoId}`).expect(404);
       });
     });
+
     describe("given the todos do exist", () => {
       it("should return 200 and the todo", async () => {
-        const toDoObject = {
-          name: "Test",
-          description: "Test",
-          date: new Date(),
-          author: "correo@dominio.com"
-        };
-        const toDo = await ToDo.create(toDoObject);
+        const toDo = await ToDo.create(mockToDoPayloads[1]);
         const { body, statusCode } = await supertest(app).get(
           `${paths.todo}/${toDo.id}`
         );
-        // console.log(body);
+
         expect(statusCode).toBe(200);
         expect(body.toDo.id).toBe(toDo.id);
       });
     });
   });
+
   describe("post todo route", () => {
     describe("given the todo creation data", () => {
-      test("should return 200 and the todo", async () => {
+      it("should return 200 and the todo", async () => {
         const { statusCode, body } = await supertest(app)
           .post(`${paths.todo}/`)
           .send(mockToDoPayloads[0]);
@@ -82,29 +101,93 @@ describe("todo", () => {
       });
     });
   });
+
   describe("put todo route", () => {
     describe("given the id is valid", () => {
       it("should return a todo", async () => {
         const id = 1;
-        const toDoWithId = { ...mockToDoPayloads[1], id };
+        const mymock = todoDAO.updateToDo.mockResolvedValueOnce({
+          ...mockToDoPayloads[1],
+          id
+        });
+
         const { statusCode, body } = await supertest(app)
           .put(`${paths.todo}/${id}`)
           .send(mockToDoPayloads[1]);
+
         expect(statusCode).toBe(200);
-        // console.log(body, statusCode);
+        expect(body).toEqual({ toDo: { ...mockToDoPayloads[1], id } });
+        mymock.mockReset();
       });
     });
 
-    describe.skip("given the id does not match", () => {
-      it("should return a 400", async () => {});
+    describe("given the id does not match", () => {
+      it("should return a 400", async () => {
+        // const invalidId = "abc";
+
+        const { statusCode, body } = await supertest(app).put(
+          `${paths.todo}/${invalidId}`
+        );
+
+        expect(statusCode).toBe(400);
+        expect(body).toEqual(mockExpectedErrors[0]);
+      });
     });
-    describe.skip("given the todo service throws", () => {
-      it("should handle the error", async () => {});
+
+    describe("given the todo service throws", () => {
+      it("should handle the error", async () => {
+        const notFound = mockExpectedErrors[1];
+
+        const { statusCode, body } = await supertest(app)
+          .put(`${paths.todo}/${notFoundId}`)
+          .send(mockToDoPayloads[1]);
+
+        expect(statusCode).toBe(404);
+        expect(body).toEqual(notFound);
+      });
     });
   });
-  describe("delete todo route", () => {});
-});
 
-afterAll(() => {
-  server.close();
+  describe("delete todo route", () => {
+    describe("given the id is valid", () => {
+      it("should return the deleted object", async () => {
+        toDo = await db.ToDo.create(mockToDoPayloads[0]);
+        toDo = toDo.get({ plain: true });
+        toDo.date = toDo.date.toISOString();
+
+        const { statusCode, body } = await supertest(app).del(
+          `${paths.todo}/${toDo.id}`
+        );
+
+        expect(statusCode).toBe(200);
+        expect({ toDo: toDo }).toEqual(body);
+      });
+    });
+
+    describe("given the id does not match", () => {
+      it("should return a 400", async () => {
+        // const invalidId = "abc";
+
+        const { statusCode, body } = await supertest(app).del(
+          `${paths.todo}/${invalidId}`
+        );
+
+        expect(statusCode).toBe(400);
+        expect(body).toEqual(mockExpectedErrors[0]);
+      });
+    });
+
+    describe("given the todo service throws", () => {
+      it("should handle the error", async () => {
+        const notFound = mockExpectedErrors[1];
+
+        const { statusCode, body } = await supertest(app).del(
+          `${paths.todo}/${notFoundId}`
+        );
+
+        expect(statusCode).toBe(404);
+        expect(body).toEqual(notFound);
+      });
+    });
+  });
 });
